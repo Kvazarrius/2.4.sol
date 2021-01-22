ee5941d5a6b158604acac3f44b386e2c33387322
Kvazar <toskla91.91@mail.ru>
2021-01-22 18:37:37 +0300

    Initial commit

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,34 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.service.UserService;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "/lk")
+    public String getUserPage2(ModelMap modelMap, Principal principal) {
+        modelMap.addAttribute("user", userService.loadUserByUsername(principal.getName()));
+        return "userPage";
+    }
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Long id, ModelMap modelMap) {
+        modelMap.addAttribute("user", userService.getUserById(id));
+        return "userPage";
+    }
+}
